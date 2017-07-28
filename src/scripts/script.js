8516a71894974d09f3e11c8a77d5df3ea4e90b38
Vladislav Klochkov <kvladislavo2013@gmail.com>
2017-07-28 13:51:48 +0300

    Removed unnecessary comments.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,20 +9,6 @@
       if($(this).data('type') === 'css') {
         $('.slider').css('display', 'none');
         $('.css-slider').css('display', 'block');
-        // $('.slider').addClass('css-only');
-        //
-        // $imageTitleList = $('div.section-switch');
-        // $imageTitleListItems = $imageTitleList.find('ul li');
-        //
-        // for(var i = 0; i < $imageTitleList.find('ul').children().length; ++i) {
-        //   $imageTitleListItems.eq(i).html('<a href="#image' + i + '">' + $imageTitleListItems.eq(i).text() + '</a>');
-        // }
-        //
-        // $imageSlider = $('div.image-slider');
-        //
-        // for(var i = $imageSlider.find('ul').children().length - 1; i >= 0; --i) {
-        //   $imageSlider.prepend('<a id="image' + i + '"></a>');
-        // }
       }
     })
   });
